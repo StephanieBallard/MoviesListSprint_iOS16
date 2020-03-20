90b50843f512426632468d209fc5bf9e7f6755df
Stephanie Ballard <user.name>
2020-03-20 08:37:17 -0700

    model and class files created and classes assigned appropriately in the storyboard

---
+++ MovieListSprint_iOS16/AddMovieViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddMovieViewController.swift
+//  MovieListSprint_iOS16
+//
+//  Created by Stephanie Ballard on 3/20/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+import UIKit
+
+class AddMovieViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MovieListSprint_iOS16/Movie.swift
@@ -0,0 +1,12 @@
+//
+//  Movie.swift
+//  MovieListSprint_iOS16
+//
+//  Created by Stephanie Ballard on 3/20/20.
+//  Copyright © 2020 Stephanie Ballard. All rights reserved.
+//
+
+struct Movie {
+    let name: String
+    var isSeen: Bool = false
+}
